7dd82ba31b925f9bad3d2b24e97437f674c96137
PVigar88 <Pvigar@gmail.com>
2021-08-03 23:52:45 -0400

    added auth router endpoints

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,12 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
+const bcrypt = require("bcrypt");
 
+//NEED TO ADD IN MIDDLEWARES
+
+const router = require("express").Router();
+
+const User = require("../users/users-model.js");
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +31,15 @@
   }
  */
 
+router.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+
+  const passHash = bcrypt.hashSync(password, 6);
+
+  User.add({ username, password: passHash }).then(({ username }) => {
+    res.status(201).json({ message: `Welcome to the party, ${username}!` });
+  });
+});
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +57,21 @@
   }
  */
 
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+
+  User.findBy({ username })
+    .first()
+    .then((user) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+
+        res.json({ message: `Welcome back, ${username}!` });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    });
+});
 
 /**
   3 [GET] /api/auth/logout
@@ -59,5 +89,23 @@
   }
  */
 
- 
+router.get("/logout", (req, res) => {
+  if (req.session.user) {
+    const { username } = req.session.user;
+
+    req.session.destroy((err) => {
+      if (err) {
+        res.json({ message: " You are unable to logout at this time" });
+      } else {
+        res.json({
+          message: `${username} has been successfully logged out, See you next time!`,
+        });
+      }
+    });
+  } else {
+    res.status(401).json({ message: "failed to logout" });
+  }
+});
+
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
